992f2af6d6f01765e83e3af21dba0cddf0635f00
pvgmoon <pandreavanegasg@gmail.com>
2023-12-27 15:59:36 -0500

    Ajuste fuente customerDay

--- src/screens/CustomerDay.jsx
+++ src/screens/CustomerDay.jsx
@@ -1,44 +1,68 @@
-import React from "react";
-import { View, Text, TouchableOpacity, Platform, Dimensions } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import Svg, { Circle, Text as SvgText } from "react-native-svg";
+import React from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import Svg, { Circle, Text as SvgText } from 'react-native-svg'
 import { useNavigation } from '@react-navigation/native'
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient'
 
-import { GlobalStyles, colors } from "../Styles/GlobalStyles";
-import { CustomerDayStyles } from "../Styles/CustomerDayStyles";
+import { GlobalStyles, colors } from '../Styles/GlobalStyles'
+import { CustomerDayStyles } from '../Styles/CustomerDayStyles'
 
 const CustomerDayPacking = () => {
   const navigation = useNavigation()
-  const isIOS = Platform.OS === "ios";
-  const { width, height } = Dimensions.get('window');
+  const isIOS = Platform.OS === 'ios'
+  const { width, height } = Dimensions.get('window')
 
-  const radius = 40;
-  const strokeWidth = 10;
-  const circumference = 2 * Math.PI * radius;
-  const percentage = 60;
-  const strokeDashoffset = circumference - (percentage / 100) * circumference;
+  const radius = 40
+  const strokeWidth = 10
+  const circumference = 2 * Math.PI * radius
+  const percentage = 60
+  const strokeDashoffset = circumference - (percentage / 100) * circumference
 
   const handleNavigateToProducts = () => {
-    navigation.navigate('Products'); 
-  };
+    navigation.navigate('Products')
+  }
 
   return (
     <LinearGradient
-    colors={['#00478C', '#026CD2']}
+      colors={['#00478C', '#026CD2']}
       style={{ flex: 1 }}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
     >
-      <View style={{ flex: 1, backgroundColor: colors.bluePrimary, paddingLeft: 0 }}>
-        <View style={{ flex: 1, alignItems: 'center', paddingTop: 60, paddingLeft: 0 }}>
+      <View
+        style={{ flex: 1, backgroundColor: colors.bluePrimary, paddingLeft: 0 }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            paddingTop: 60,
+            paddingLeft: 0,
+          }}
+        >
           <View
             style={{
-              ...CustomerDayStyles.titleCard
+              ...CustomerDayStyles.titleCard,
             }}
           >
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={{ ...GlobalStyles.textBtnSecundary, fontSize: 24, fontFamily: 'PoppinsSemi', color: colors.bluePrimary }}>Customer Day</Text>
+              <Text
+                style={{
+                  ...GlobalStyles.textBtnSecundary,
+                  fontSize: 24,
+                  fontFamily: 'PoppinsSemi',
+                  color: colors.bluePrimary,
+                }}
+              >
+                Customer Day
+              </Text>
             </View>
           </View>
 
@@ -48,10 +72,30 @@ const CustomerDayPacking = () => {
             }}
           >
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <View style={{ backgroundColor: "white", borderRadius: 30, padding: 8, marginRight: 10 }}>
-                <Ionicons name={isIOS ? 'ios-search' : 'md-search'} size={24} color="#00478C" />
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  borderRadius: 30,
+                  padding: 8,
+                  marginRight: 10,
+                }}
+              >
+                <Ionicons
+                  name={isIOS ? 'ios-search' : 'md-search'}
+                  size={24}
+                  color="#00478C"
+                />
               </View>
-              <Text style={{ ...GlobalStyles.textBtnSecundary, fontSize: 18, fontFamily: 'Poppins', color: colors.bluePrimary }}>Búsqueda</Text>
+              <Text
+                style={{
+                  ...GlobalStyles.textBtnSecundary,
+                  fontSize: 18,
+                  fontFamily: 'PoppinsRegular',
+                  color: colors.bluePrimary,
+                }}
+              >
+                Búsqueda
+              </Text>
             </View>
           </View>
 
@@ -63,7 +107,14 @@ const CustomerDayPacking = () => {
               }}
               onPress={handleNavigateToProducts}
             >
-              <View style={{ width: '20%', justifyContent: 'center', alignItems: 'center', margin: 10 }}>
+              <View
+                style={{
+                  width: '20%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  margin: 10,
+                }}
+              >
                 <Svg
                   style={{ width: '100%' }}
                   height={radius * 2}
@@ -89,15 +140,50 @@ const CustomerDayPacking = () => {
                     strokeDasharray={`${circumference} ${circumference}`}
                     strokeDashoffset={strokeDashoffset}
                   />
-                  <SvgText x={radius-6} y={radius + 6} textAnchor="middle" stroke="#00478C" fontSize="18" fontFamily="Poppins" fill="#00478C">
+                  <SvgText
+                    x={radius - 6}
+                    y={radius + 6}
+                    textAnchor="middle"
+                    stroke="#00478C"
+                    fontSize="18"
+                    fontFamily="PoppinsRegular"
+                    fill="#00478C"
+                  >
                     {percentage}%
                   </SvgText>
                 </Svg>
               </View>
-              <View style={{ width: '110%', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end' }}>
-                <View style={{ width: '90%', alignItems: 'left', marginBottom:10 }}>
-                  <Text style={{ ...GlobalStyles.textBtnSecundary, fontSize: 16, fontFamily: 'Poppins', color: colors.darkBlue }}>Customer 1:</Text>
-                  <Text style={{ ...GlobalStyles.textBtnSecundary, fontSize: 14, fontFamily: 'Poppins', color: colors.darkBlue }}>Tech Point 1. Grownet</Text>
+              <View
+                style={{
+                  width: '110%',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'flex-end',
+                }}
+              >
+                <View
+                  style={{ width: '90%', alignItems: 'left', marginBottom: 10 }}
+                >
+                  <Text
+                    style={{
+                      ...GlobalStyles.textBtnSecundary,
+                      fontSize: 16,
+                      fontFamily: 'PoppinsRegular',
+                      color: colors.darkBlue,
+                    }}
+                  >
+                    Customer 1:
+                  </Text>
+                  <Text
+                    style={{
+                      ...GlobalStyles.textBtnSecundary,
+                      fontSize: 14,
+                      fontFamily: 'PoppinsRegular',
+                      color: colors.darkBlue,
+                    }}
+                  >
+                    Tech Point 1. Grownet
+                  </Text>
                 </View>
               </View>
             </TouchableOpacity>
@@ -105,7 +191,7 @@ const CustomerDayPacking = () => {
         </View>
       </View>
     </LinearGradient>
-  );
-};
+  )
+}
 
-export default CustomerDayPacking;
+export default CustomerDayPacking
