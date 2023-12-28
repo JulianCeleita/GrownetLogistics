21ac3f2b008e1a9e016480d4f49139860d263af7
liamgabriel2021 <arbey221@gmail.com>
2023-12-28 16:50:31 -0500

    creacion de archivo urls config

---
+++ src/config/urls.config.js
@@ -0,0 +1,6 @@
+export const productHasOrder = process.env.EXPO_PUBLIC_PRODUCT_HAS_ORDER
+
+export const insertPacking = process.env.EXPO_PUBLIC_PACKING_INSERT
+export const onlyCountries = process.env.EXPO_PUBLIC_COUNTRIES
+export const orderWithStatus2 = process.env.EXPO_PUBLIC_ORDER_WITH_STATUS2
+export const insertLoading = process.env.EXPO_PUBLIC_INSERT_LOADING
