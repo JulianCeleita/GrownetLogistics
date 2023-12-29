73d2bf749d4dcccbdf115a64f543aba974ee05be
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2023-12-29 16:08:07 -0500

    Cambios en las variables de entorno

--- src/config/urls.config.js
+++ src/config/urls.config.js
@@ -1,7 +1,6 @@
-export const productHasOrder = process.env.EXPO_PUBLIC_PRODUCT_HAS_ORDER
 //PACKING
 export const productsPacking = process.env.EXPO_PUBLIC_PRODUCTS_PACKING
-export const insertPacking = process.env.EXPO_PUBLIC_PACKING_INSERT
+export const insertPacking = process.env.EXPO_PUBLIC_INSERT_PACKING
 //LOADING
 export const productsLoading = process.env.EXPO_PUBLIC_PRODUCTS_LOADING
 export const insertLoading = process.env.EXPO_PUBLIC_INSERT_LOADING
--- src/store/usePackingStore.js
+++ src/store/usePackingStore.js
@@ -1,12 +1,13 @@
 import { create } from 'zustand'
 import mainAxios from '../../axios.Config';
-import { productHasOrder } from '../config/urls.config'
+import { productsPacking } from '../config/urls.config'
 
 export const usePackingStore = create((set) => ({
     packingProducts: [],
     setProducts: async (token, accountNumber) => {
+
         try {
-            const resp = await mainAxios.get(`${productHasOrder}${accountNumber}`, {
+            const resp = await mainAxios.get(`${productsPacking}${accountNumber}`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 }
