028786e231a53ee2e2d4c528b13fcb0cda300009
Danna Maya <dxmayac@uqvirtual.edu.co>
2024-01-03 18:27:14 -0500

    Cambios en vista Date

--- src/screens/CustomDate.jsx
+++ src/screens/CustomDate.jsx
@@ -1,43 +1,45 @@
-import { useNavigation } from '@react-navigation/native'
-import moment from 'moment'
-import React, { useEffect, useState } from 'react'
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
-import FechaIcon from '../img/Fecha.png'
-
-import { CustomDateStyles } from '../styles/CustomDateStyles'
-import useOrdersByDate from '../store/useOrdersByDateStore'
-import useTokenStore from '../store/useTokenStore'
+import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View, Animated, SafeAreaView } from 'react-native';
+import FechaIcon from '../img/Fecha.png';
+import { CustomDateStyles } from '../styles/CustomDateStyles';
+import { CustomerDayStyles } from '../styles/CustomerDayStyles';
 
 const CustomDate = () => {
-  const [currentDate, setCurrentDate] = useState(moment().format('MMM DD'))
-  const [currentDay, setCurrentDay] = useState(moment().format('dddd'))
-  const [showMore, setShowMore] = useState(false)
-  const navigation = useNavigation()
-  const { setOrdersByDate } = useOrdersByDate()
-  const { token } = useTokenStore()
+  const [animation] = useState(new Animated.Value(1));
+  const [currentDate, setCurrentDate] = useState(moment().format('MMM DD'));
+  const [currentDay, setCurrentDay] = useState(moment().format('dddd'));
+  const [showMore, setShowMore] = useState(false);
+  const navigation = useNavigation();
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentDate(moment().format('MMM DD'))
-      setCurrentDay(moment().format('dddd'))
-    }, 1000)
+      setCurrentDate(moment().format('MMM DD'));
+      setCurrentDay(moment().format('dddd'));
+    }, 1000);
 
     return () => {
-      clearInterval(interval)
-    }
-  }, [])
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
-    setOrdersByDate(token)
-  }, [])
+    Animated.timing(animation, {
+      toValue: showMore ? 1 : 0,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }, [showMore]);
 
   const handleDatePress = () => {
-    console.log('Button pressed: Date')
-    navigation.navigate('MyTabs')
-  }
+    console.log('Button pressed: Date');
+    navigation.navigate('PackingScreen');
+  };
 
   const handleShowMore = () => {
-    setShowMore(!showMore)
-  }
+    setShowMore(!showMore);
+  };
 
   const renderButton = (date) => {
     return (
@@ -54,25 +56,37 @@ const CustomDate = () => {
           <Text style={CustomDateStyles.buttonText}>{date}</Text>
         </View>
       </View>
-    )
-  }
+    );
+  };
 
   const renderAdditionalButtons = () => {
     if (showMore) {
-      const initialDate = moment().add(1, 'days')
-      const dates = []
+      const initialDate = moment().add(1, 'days');
+      const dates = [];
 
       for (let i = 0; i < 2; i++) {
-        // Modificación aquí: cambiar el límite a 2 iteraciones
         const nextDate = initialDate
           .clone()
           .add(i, 'days')
-          .format('ddd, MMM DD')
-        dates.push(nextDate)
+          .format('ddd, MMM DD');
+        dates.push(nextDate);
       }
 
       return (
-        <View style={{ alignItems: 'center' }}>
+        <Animated.View
+          style={{
+            alignItems: 'center',
+            opacity: animation,
+            transform: [
+              {
+                translateY: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-100, 0],
+                }),
+              },
+            ],
+          }}
+        >
           {dates.map((date, index) => (
             <View
               key={index}
@@ -81,42 +95,53 @@ const CustomDate = () => {
               {renderButton(date)}
             </View>
           ))}
-        </View>
-      )
+        </Animated.View>
+      );
     } else {
-      return null
+      return null;
     }
-  }
+  };
+
   return (
-    <View style={CustomDateStyles.container}>
-      <ScrollView contentContainerStyle={CustomDateStyles.contentContainer}>
-        <TouchableOpacity
-          style={CustomDateStyles.whiteBackground}
-          onPress={handleDatePress}
-        >
-          <View style={CustomDateStyles.dateButtonContainer}>
-            <View style={CustomDateStyles.dateButton}>
-              <Image source={FechaIcon} style={{ width: 50, height: 50 }} />
-            </View>
-          </View>
-          <View style={CustomDateStyles.dateTextContainer}>
-            <Text style={CustomDateStyles.buttonText}>
-              {currentDay}, {currentDate}
-            </Text>
-          </View>
-        </TouchableOpacity>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <ScrollView>
+        <View style={CustomerDayStyles.title2}>
+          <Text style={[CustomerDayStyles.customerTitle, { marginTop: Platform.OS === 'ios' ? null : 25, fontSize: Platform.OS === 'ios' ? 27 : 25 }]}>Date</Text>
+        </View>
 
-        {renderAdditionalButtons()}
+        <View style={CustomDateStyles.container}>
+          <ScrollView contentContainerStyle={CustomDateStyles.contentContainer}>
+            <TouchableOpacity
+              style={CustomDateStyles.whiteBackground}
+              onPress={handleDatePress}
+            >
+              <View style={CustomDateStyles.dateButtonContainer}>
+                <View style={CustomDateStyles.dateButton}>
+                  <Image source={FechaIcon} style={{ width: 50, height: 50 }} />
+                </View>
+              </View>
+              <View style={CustomDateStyles.dateTextContainer}>
+                <Text style={CustomDateStyles.buttonText}>
+                  {currentDay}, {currentDate}
+                </Text>
+              </View>
+            </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={handleShowMore}
-          style={CustomDateStyles.showMoreButton}
-        >
-          <Text>Show more</Text>
-        </TouchableOpacity>
+            {renderAdditionalButtons()}
+
+            <TouchableOpacity
+              onPress={handleShowMore}
+              style={CustomDateStyles.showMoreButton}
+            >
+              <Text style={CustomDateStyles.showMoreButtonText}>
+                {showMore ? 'Hide' : 'Show more'}
+              </Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </View>
       </ScrollView>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default CustomDate
+export default CustomDate;
