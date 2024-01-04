c187ed51a4eb326e72562e2c4c5f691a0c105f70
liamgabriel2021 <arbey221@gmail.com>
2024-01-03 19:00:48 -0500

    ruta a mytabs, corregida

--- src/screens/CustomDate.jsx
+++ src/screens/CustomDate.jsx
@@ -1,45 +1,53 @@
-import { useNavigation } from '@react-navigation/native';
-import moment from 'moment';
-import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, Text, TouchableOpacity, View, Animated, SafeAreaView } from 'react-native';
-import FechaIcon from '../img/Fecha.png';
-import { CustomDateStyles } from '../styles/CustomDateStyles';
-import { CustomerDayStyles } from '../styles/CustomerDayStyles';
+import { useNavigation } from '@react-navigation/native'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import {
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Animated,
+  SafeAreaView,
+} from 'react-native'
+import FechaIcon from '../img/Fecha.png'
+import { CustomDateStyles } from '../styles/CustomDateStyles'
+import { CustomerDayStyles } from '../styles/CustomerDayStyles'
 
 const CustomDate = () => {
-  const [animation] = useState(new Animated.Value(1));
-  const [currentDate, setCurrentDate] = useState(moment().format('MMM DD'));
-  const [currentDay, setCurrentDay] = useState(moment().format('dddd'));
-  const [showMore, setShowMore] = useState(false);
-  const navigation = useNavigation();
+  const [animation] = useState(new Animated.Value(1))
+  const [currentDate, setCurrentDate] = useState(moment().format('MMM DD'))
+  const [currentDay, setCurrentDay] = useState(moment().format('dddd'))
+  const [showMore, setShowMore] = useState(false)
+  const navigation = useNavigation()
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentDate(moment().format('MMM DD'));
-      setCurrentDay(moment().format('dddd'));
-    }, 1000);
+      setCurrentDate(moment().format('MMM DD'))
+      setCurrentDay(moment().format('dddd'))
+    }, 1000)
 
     return () => {
-      clearInterval(interval);
-    };
-  }, []);
+      clearInterval(interval)
+    }
+  }, [])
 
   useEffect(() => {
     Animated.timing(animation, {
       toValue: showMore ? 1 : 0,
       duration: 300,
       useNativeDriver: false,
-    }).start();
-  }, [showMore]);
+    }).start()
+  }, [showMore])
 
   const handleDatePress = () => {
-    console.log('Button pressed: Date');
-    navigation.navigate('PackingScreen');
-  };
+    console.log('Button pressed: Date')
+    navigation.navigate('MyTabs')
+  }
 
   const handleShowMore = () => {
-    setShowMore(!showMore);
-  };
+    setShowMore(!showMore)
+  }
 
   const renderButton = (date) => {
     return (
@@ -56,20 +64,20 @@ const CustomDate = () => {
           <Text style={CustomDateStyles.buttonText}>{date}</Text>
         </View>
       </View>
-    );
-  };
+    )
+  }
 
   const renderAdditionalButtons = () => {
     if (showMore) {
-      const initialDate = moment().add(1, 'days');
-      const dates = [];
+      const initialDate = moment().add(1, 'days')
+      const dates = []
 
       for (let i = 0; i < 2; i++) {
         const nextDate = initialDate
           .clone()
           .add(i, 'days')
-          .format('ddd, MMM DD');
-        dates.push(nextDate);
+          .format('ddd, MMM DD')
+        dates.push(nextDate)
       }
 
       return (
@@ -96,17 +104,27 @@ const CustomDate = () => {
             </View>
           ))}
         </Animated.View>
-      );
+      )
     } else {
-      return null;
+      return null
     }
-  };
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <ScrollView>
         <View style={CustomerDayStyles.title2}>
-          <Text style={[CustomerDayStyles.customerTitle, { marginTop: Platform.OS === 'ios' ? null : 25, fontSize: Platform.OS === 'ios' ? 27 : 25 }]}>Date</Text>
+          <Text
+            style={[
+              CustomerDayStyles.customerTitle,
+              {
+                marginTop: Platform.OS === 'ios' ? null : 25,
+                fontSize: Platform.OS === 'ios' ? 27 : 25,
+              },
+            ]}
+          >
+            Date
+          </Text>
         </View>
 
         <View style={CustomDateStyles.container}>
@@ -141,7 +159,7 @@ const CustomDate = () => {
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default CustomDate;
+export default CustomDate
