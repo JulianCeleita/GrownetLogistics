95e9b795605a34dab2ed0f32eda523beb30f076d
liamgabriel2021 <arbey221@gmail.com>
2024-01-05 08:32:19 -0500

    loading items card para confirmar productos

--- src/hooks/useProductSubmit.jsx
+++ src/hooks/useProductSubmit.jsx
@@ -11,7 +11,7 @@ export const useProductSubmit = () => {
 
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = (itemId, quantity = 0, note = '') => {
+  const handleSubmit = async (itemId, quantity = 0, note = '') => {
     setIsLoading(true)
 
     const data = {
@@ -22,33 +22,26 @@ export const useProductSubmit = () => {
 
     console.log('data', data)
 
-    const promise = new Promise((resolve, reject) => {
-      mainAxios
-        .post(insertPacking, data, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            console.log('Datos enviados correctamente', response.data)
-            setError(null)
-            resolve()
-          } else {
-            throw new Error('Error al enviar los datos')
-          }
-        })
-        .catch((error) => {
-          console.error('Hubo un error al enviar los datos: ', error)
-          setError(error)
-          reject(error)
-        })
-        .finally(() => {
-          setIsLoading(false)
-        })
-    })
+    try {
+      const response = await mainAxios.post(insertPacking, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
-    promiseQueue.push(promise)
+      if (response.status === 200) {
+        console.log('Datos enviados correctamente', response.data)
+        setError(null)
+      } else {
+        throw new Error('Error al enviar los datos')
+      }
+    } catch (error) {
+      console.error('Hubo un error al enviar los datos: ', error)
+      setError(error)
+      throw error
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   const executeNextPromise = () => {
