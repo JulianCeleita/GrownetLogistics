2c3e3b1eb5c45f93d75593dd1f651612dc1d235f
Danna Maya <dxmayac@uqvirtual.edu.co>
2024-01-11 16:09:10 -0500

    Cambios en clasificación de productVan, useShortVanStore y estilos

--- src/screens/ShortsVan/ProductVan.jsx
+++ src/screens/ShortsVan/ProductVan.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, FlatList, Text, TouchableOpacity, View, Platform } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons'
+import { CustomerDayStyles } from '../../styles/CustomerDayStyles'
 import ProductSearcher from '../../components/ProductSearch'
 import { useShortVanStore } from '../../store/useShortVanStore'
 import useEmployeeStore from '../../store/useEmployeeStore'
@@ -10,24 +11,13 @@ import { colors } from '../../styles/GlobalStyles'
 import { ProductStyles } from '../../styles/ProductStyles'
 
 function ProductsVan() {
-  const { restaurantProducts, setFetchShortVanProducts } = useShortVanStore();
+  const { restaurantData, loading, error, setFetchShortVanProducts } = useShortVanStore();
   const [search, setSearch] = useState(false);
-  const [vansList, setVansList] = useState([]);
-  const { employeeToken } = useEmployeeStore()
+  const { employeeToken } = useEmployeeStore();
 
   useEffect(() => {
     setFetchShortVanProducts(employeeToken);
   }, []);
-
-  useEffect(() => {
-    if (restaurantProducts && Object.keys(restaurantProducts).length > 0) {
-      const vans = Object.entries(restaurantProducts).map(([vanName, vanProducts]) => ({
-        vanName,
-        vanProducts,
-      }));
-      setVansList(vans);
-    }
-  }, [restaurantProducts]);
 
   const handleSearch = () => {
     setSearch(true);
@@ -38,45 +28,48 @@ function ProductsVan() {
       {search ? (
         <ProductSearcher setSearch={setSearch} />
       ) : (
-        <TouchableOpacity onPress={handleSearch} style={ProductStyles.icon2}>
-          <Ionicons name="md-search-circle-outline" size={35} color={colors.darkBlue} />
-        </TouchableOpacity>
+        <View style={CustomerDayStyles.title2}>
+          <Text style={CustomerDayStyles.customerTitle}>Route 1</Text>
+          <TouchableOpacity onPress={handleSearch} style={ProductStyles.icon2}>
+            <Ionicons name="md-search-circle-outline" size={35} color={colors.darkBlue} />
+          </TouchableOpacity>
+        </View>
       )}
 
-      <FlatList
-        data={vansList}
-        renderItem={({ item }) => (
-          <View key={item.vanName}>
-            <Text
-              style={[
-                ProductStyles.customerTitle,
-                {
-                  color: colors.bluePrimary,
-                  marginTop: Platform.OS === 'ios' ? 9 : 16,
-                  marginBottom: Platform.OS === 'ios' ? 10 : null,
-                },
-              ]}
-            >
-              Van: {item.vanName}
-            </Text>
-            <FlatList
-              data={item.vanProducts}
-              renderItem={({ item: product, index }) => (
-                <ProductsCardBulkVan key={index} item={product} />
-              )}
-              keyExtractor={(product) => product.id.toString()}
-              ListFooterComponent={<View style={{ height: 60 }} />}
-            />
-          </View>
-        )}
-        keyExtractor={(item) => item.vanName}
-        ListFooterComponent={<View style={{ height: 60 }} />}
-        ListEmptyComponent={
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ActivityIndicator size="large" color="#0000ff" />
-          </View>
-        }
-      />
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : error ? (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Error: {error}</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={restaurantData}
+          renderItem={({ item: restaurant }) => (
+            <View key={restaurant.vanName} style={{ marginBottom: -45 }}>
+              <Text
+                style={[
+                  CustomerDayStyles.restaurantName,
+                  {
+                    marginBottom: Platform.OS === 'ios' ? 5 : null,
+                  },
+                ]}
+              >
+                {restaurant.vanName}
+              </Text>
+              <FlatList
+                data={restaurant.vanProducts}
+                renderItem={({ item: product, index }) => (
+                  <ProductsCardBulkVan key={index} item={product} />
+                )}
+                keyExtractor={(product) => product.id.toString()}
+                ListFooterComponent={<View style={{ height: 60 }} />}
+              />
+            </View>
+          )}
+          keyExtractor={(restaurant) => restaurant.vanName}
+        />
+      )}
     </SafeAreaView>
   );
 }
