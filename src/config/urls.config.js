0e5bbf398605b5317693e1976c042839ee1db972
Julian Celeita <andresceleita@yahoo.es>
2024-01-12 18:31:51 -0500

    Se elimina endpoint sin uso

--- src/config/urls.config.js
+++ src/config/urls.config.js
@@ -11,8 +11,6 @@ export const insertPacking = process.env.EXPO_PUBLIC_INSERT_PACKING
 //LOADING
 export const productsLoadingConfig = process.env.EXPO_PUBLIC_PRODUCTS_LOADING
 export const insertLoading = process.env.EXPO_PUBLIC_INSERT_LOADING
-//ORDERS_BY_DATE  TODO:cambiar con la api cuando ya este ordenes por fecha.
-export const allOrdersByDate = process.env.EXPO_PUBLIC_ORDERS_BY_DATE
 //PERCENTAGE
 export const percentagePacking = process.env.EXPO_PUBLIC_PERCENTAGE_PACKING
 export const percentageLoading = process.env.EXPO_PUBLIC_PERCENTAGE_LOADING
