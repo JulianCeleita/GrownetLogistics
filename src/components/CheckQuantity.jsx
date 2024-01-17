db383852dcbac6680e31801a9b32ba22d8a037ba
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-01-16 19:18:07 -0500

    Nuevo componente para el manejo del Missing y el Overweight

---
+++ src/components/CheckQuantity.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import { ProductStyles } from '../styles/ProductStyles'
+import { Text } from 'react-native'
+import { colors } from '../styles/GlobalStyles'
+
+export const CheckQuantity = ({ viewPacking, quantity, quantity_packing, quantity_loading, packed }) => {
+    const [message, setMessage] = useState('')
+
+    useEffect(() => {
+        if (!packed) {
+            if (viewPacking) {
+                setMessage(
+                    quantity > quantity_packing
+                        ? `Missing ${quantity - quantity_packing}`
+                        : quantity < quantity_packing
+                            ? `Overweight ${quantity_packing - quantity}`
+                            : ''
+                )
+            } else {
+                setMessage(
+                    quantity > quantity_loading
+                        ? `Missing ${quantity - quantity_loading}`
+                        : quantity < quantity_loading
+                            ? `Overweight ${quantity_loading - quantity}`
+                            : ''
+                )
+            }
+        } else {
+            setMessage(
+                quantity > packed
+                    ? `Missing ${quantity - packed}`
+                    : quantity < packed
+                        ? `Overweight ${packed - quantity}`
+                        : ''
+            )
+        }
+
+
+    }, [viewPacking, quantity_packing, quantity_loading, packed])
+
+    return (
+        <Text
+            style={[
+                ProductStyles.textCard,
+                {
+                    color: message.includes('Missing')
+                        ? colors.danger
+                        : colors.green,
+                    marginRight: 50
+                },
+            ]}
+        >
+            {message}
+        </Text>
+    )
+}
