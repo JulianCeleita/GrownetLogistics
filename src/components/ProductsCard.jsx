386b3efb08c0f5f4eb9510e2cb7c8d81c40ef442
pvgmoon <pandreavanegasg@gmail.com>
2024-01-25 16:20:47 -0500

    Color azul N/A e icono

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -122,8 +122,6 @@ export function ProductsCard({
                 colorPress={colorPress}
                 colorRight={colorRight}
                 colorLeft={colorLeft}
-                declareNotAvailable={declareNotAvailable}
-                handleSubmit={handleSubmit}
               />
             </View>
 
