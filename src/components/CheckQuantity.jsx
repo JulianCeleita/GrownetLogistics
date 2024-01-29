2cc44e18795ca8c81cc49b22b08114bad15919f5
liamgabriel2021 <arbey221@gmail.com>
2024-01-29 10:04:40 -0500

    los NA se ven en todas las vistas, solo se puede modifcar desde short van

--- src/components/CheckQuantity.jsx
+++ src/components/CheckQuantity.jsx
@@ -3,88 +3,86 @@ import { ProductStyles } from '../styles/ProductStyles'
 import { Text } from 'react-native'
 import { colors } from '../styles/GlobalStyles'
 
-export const CheckQuantity = ({ viewPacking, quantity, quantity_packing, quantity_loading, packed }) => {
-    const [message, setMessage] = useState('')
+export const CheckQuantity = ({
+  viewPacking,
+  quantity,
+  quantity_packing,
+  quantity_loading,
+  packed,
+}) => {
+  const [message, setMessage] = useState('')
 
-    console.log({ quantity, quantity_packing, quantity_loading });
-
-    useEffect(() => {
-        if (!packed) {
-            if (viewPacking) {
-                // Vista de packing
-                if (quantity_loading && quantity_packing) {
-                    setMessage(
-                        quantity > quantity_loading
-                            ? `Missing ${quantity - quantity_loading}`
-                            : quantity < quantity_loading
-                                ? `Overweight ${quantity_loading - quantity}`
-                                : ''
-                    )
-                } else if (!quantity_packing && quantity_loading) {
-                    setMessage(
-                        quantity > quantity_loading
-                            ? `Missing ${quantity - quantity_loading}`
-                            : quantity < quantity_loading
-                                ? `Overweight ${quantity_loading - quantity}`
-                                : ''
-                    )
-                } else if (!quantity_loading && quantity_packing) {
-                    setMessage(
-                        quantity > quantity_packing
-                            ? `Missing ${quantity - quantity_packing}`
-                            : quantity < quantity_packing
-                                ? `Overweight ${quantity_packing - quantity}`
-                                : ''
-                    )
-
-                }
-
-            } else {
-                // Vista de loading
-                if (quantity_packing && !quantity_loading) {
-                    setMessage(
-                        quantity > quantity_packing
-                            ? `Missing ${quantity - quantity_packing}`
-                            : quantity < quantity_packing
-                                ? `Overweight ${quantity_packing - quantity}`
-                                : ''
-                    )
-                } else if (quantity_loading && quantity_packing) {
-                    setMessage(
-                        quantity > quantity_loading
-                            ? `Missing ${quantity - quantity_loading}`
-                            : quantity < quantity_loading
-                                ? `Overweight ${quantity_loading - quantity}`
-                                : ''
-                    )
-                }
-            }
-        } else {
-            setMessage(
-                quantity > packed
-                    ? `Missing ${quantity - packed}`
-                    : quantity < packed
-                        ? `Overweight ${packed - quantity}`
-                        : ''
-            )
+  useEffect(() => {
+    if (!packed) {
+      if (viewPacking) {
+        // Vista de packing
+        if (quantity_loading && quantity_packing) {
+          setMessage(
+            quantity > quantity_loading
+              ? `Missing ${quantity - quantity_loading}`
+              : quantity < quantity_loading
+                ? `Overweight ${quantity_loading - quantity}`
+                : '',
+          )
+        } else if (!quantity_packing && quantity_loading) {
+          setMessage(
+            quantity > quantity_loading
+              ? `Missing ${quantity - quantity_loading}`
+              : quantity < quantity_loading
+                ? `Overweight ${quantity_loading - quantity}`
+                : '',
+          )
+        } else if (!quantity_loading && quantity_packing) {
+          setMessage(
+            quantity > quantity_packing
+              ? `Missing ${quantity - quantity_packing}`
+              : quantity < quantity_packing
+                ? `Overweight ${quantity_packing - quantity}`
+                : '',
+          )
         }
+      } else {
+        // Vista de loading
+        if (quantity_packing && !quantity_loading) {
+          setMessage(
+            quantity > quantity_packing
+              ? `Missing ${quantity - quantity_packing}`
+              : quantity < quantity_packing
+                ? `Overweight ${quantity_packing - quantity}`
+                : '',
+          )
+        } else if (quantity_loading && quantity_packing) {
+          setMessage(
+            quantity > quantity_loading
+              ? `Missing ${quantity - quantity_loading}`
+              : quantity < quantity_loading
+                ? `Overweight ${quantity_loading - quantity}`
+                : '',
+          )
+        }
+      }
+    } else {
+      setMessage(
+        quantity > packed
+          ? `Missing ${quantity - packed}`
+          : quantity < packed
+            ? `Overweight ${packed - quantity}`
+            : '',
+      )
+    }
+  }, [viewPacking, quantity_packing, quantity_loading, packed])
 
-
-    }, [viewPacking, quantity_packing, quantity_loading, packed])
-
-    return (
-        <Text
-            style={[
-                ProductStyles.textCard,
-                {
-                    color: message.includes('Missing')
-                        ? colors.danger
-                        : colors.green,
-                    marginRight: 50
-                },
-            ]}
-        >
-            {message}
-        </Text>
-    )
+  return (
+    <Text
+      style={[
+        ProductStyles.textCard,
+        {
+          color: message.includes('Missing') ? colors.danger : colors.green,
+          marginRight: 50,
+        },
+      ]}
+    >
+      {message}
+    </Text>
+  )
 }
